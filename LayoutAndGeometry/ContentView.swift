38c56b8bd59786e68aff22f65f8bb8a2340a857c
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-03-28 20:28:41 +0500

    Challenge 2
    Made views adjust their scale depending on their vertical position, with views near the bottom being large and views near the top being small going no smaller than 50% of the regular size.

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -21,6 +21,7 @@ struct ContentView: View {
                             .background(colors[index % 7])
                             .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
                             .opacity((proxy.frame(in: .global).minY) / 200)
+                            .scaleEffect((proxy.frame(in: .global).midY) / fullView.frame(in: .global).midY > 0.5 ? (proxy.frame(in: .global).midY) / fullView.frame(in: .global).midY : 0.5)
                     }
                     .frame(height: 40)
                 }
